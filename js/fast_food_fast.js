7e6cf74c02e47ab64511b86a980226f08520151f
Francis Kiryowa <franciskiryowa68@gmail.com>
2018-09-04 01:53:04 -0400

    add feature for displaying signup form and login form when signup and login buttons are clicked [finishes #160234467]

--- js/fast_food_fast.js
+++ js/fast_food_fast.js
@@ -11,28 +11,37 @@ function display_original_test(){
     fast_food_fast_header.innerHTML = "";
     fast_food_fast_header.innerHTML = "Fast-Food-Fast";
 }
-
+//defining what happens when a user clicks the signup button
 document.getElementById("register").addEventListener("click",function(){
    var content_area = document.getElementById('content_area');
    var typewriter_div = document.getElementById('typewriter_div');
    var signup_form = document.getElementById('signup_form');
    var login_form = document.getElementById('site_login_form');
+   document.body.style.backgroundImage="url(images/background.jpg)";
+   login_form.style.display="none";
    typewriter_div.style.display = "none";
    content_area.style.display = "none";
-   login_form.style.display="none";
    signup_form.style.display = "block";
-  
-
 });
+
+//hide typewritter  div 
+function hide_typewriter_div(){
+    document.getElementById("typewriter_div").style.display="none";
+}
+
+//specifying the time the div fadesout
+//setTimeout(hide_typewriter_div,9000);
 
 document.getElementById("enter_account").addEventListener("click",function(){
     var content_area = document.getElementById('content_area');
     var typewriter_div = document.getElementById('typewriter_div');
     var signup_form = document.getElementById('signup_form');
-    var login_form = document.getElementById('site_login_form');
+    var login_user_form = document.getElementById('site_login_form');
+    document.body.style.backgroundImage="url(images/background.jpg)";
     typewriter_div.style.display = "none";
     content_area.style.display = "none";
     signup_form.style.display = "none";
-    login_form.style.display="block";
+    login_user_form.style.display="block";
     
 });
+
